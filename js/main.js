ff9e39ca34da812fb3e6c60ad57857ed557949ee
Marina <presto.m@mail.ru>
2025-01-27 19:08:46 +0300

    init-commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".gallery__btn-next",
+    prevEl: ".gallery__btn-prev",
+  },
+  breakpoints: {
+    390: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
